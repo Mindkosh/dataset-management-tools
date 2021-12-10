bd7206055e3995fc3208512fe9a5aa059b56c7c2
Sahil8485 <sahilshah040473@gmail.com>
2021-12-10 21:06:21 +0530

    brightness feature added

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 import os
 import time
-from tkinter import * 
+from tkinter import *
 import tkinter.font as font
 from tkinter.ttk import Progressbar
 from imageCanvas import ImageCanvas
@@ -22,29 +22,37 @@ class MainWindow:
         self.image_number = StringVar()
         self.image_number.set("0/0")
         self.image_number_label = Label( self.controlsFrame, textvariable=self.image_number, font=("Arial", 16), pady=8, padx=10 )
-        
+
         self.image_number_label.grid(row=0, column=1)
-        
+
         self.imageFrame = Frame(self.ws, relief=RAISED, borderwidth=1)
         self.imageFrame.grid(row=0, column=0)
         self.canvas_obj = ImageCanvas(self.imageFrame, self.ws, self.height, self.width, self.image_number)
 
         self.myFont = font.Font(size=12)
-        
+
         self.btn3 = Button(self.controlsFrame, text="Previous", command = self.canvas_obj.previous_image, bg='#0052cc', fg='#ffffff', pady=8, cursor="hand1" )
         self.btn3["font"] = self.myFont
         self.btn3.grid(row=0, column=0)
-        
+
         self.btn4 = Button(self.controlsFrame, text="Next", command = self.canvas_obj.next_image, bg='#0052cc', fg='#ffffff', pady=8, cursor="hand1" )
         self.btn4["font"] = self.myFont
         self.btn4.grid(row=0, column=2)
 
         self.createMenu()
+        self.v=DoubleVar()
+        self.scroll=Scale(self.ws,orient=HORIZONTAL,resolution=65793,label='Brightness',from_ = 0, to = 16777215, variable=self.v,command=self.control)
+        self.scroll.set(15790320)
+        self.scroll.grid( row = 15, column=1, columnspan=1, sticky='ew' )
+    def control(self,n):
+        m=int(n)
+        col='#'+( '000000' + hex( m )[2:])[~5:]
+        self.ws.tk_setPalette(col)
 
 
     def aboutWindow(self):
         print("Menu option clicked")
-    
+
 
     def load_from_dataset_file(self):
         self.popup_bonus()
@@ -80,13 +88,13 @@ class MainWindow:
         self.filemenu.add_command(label="Load from Dataset file", command=self.load_from_dataset_file, font=("Arial", 16))
         self.filemenu.add_command(label="Load Image Directory", command=self.load_from_directory, font=("Arial", 16))
         self.filemenu.add_command(label="Export Images with labels", command=self.export_labeled_images, font=("Arial", 16), state="normal")
-        
+
         self.filemenu.add_separator()
 
         self.filemenu.add_command(label="Settings", command=self.settings_dialog, font=("Arial", 16), state="normal")
         self.filemenu.add_command(label="Exit", command=self.ws.quit, font=("Arial", 16))
         self.menubar.add_cascade(label="File", menu=self.filemenu, font=("Arial", 16))
-       
+
         self.helpmenu = Menu(self.menubar, tearoff=0)
         self.helpmenu.add_command(label="About", command=self.aboutWindow, font=("Arial", 16))
         self.menubar.add_cascade(label="Help", menu=self.helpmenu, font=("Arial", 16))
@@ -95,20 +103,20 @@ class MainWindow:
 
     def popup_bonus(self):
         self.popup_window = Toplevel()
-        
+
         self.popup_window.wm_title("Loading Data")
-        
+
         self.popup_window_label = Label( self.popup_window, text="Loading Dataset...", padx=20, pady=20, font=("Arial", 16) )
         self.popup_window_label.grid(row=0, column=0)
 
 
     def popup_progress_bar(self):
         self.progress_bar_window = Toplevel()
-        
+
         self.progress_bar_window.wm_title("Exporting")
         self.progress_window_label1 = Label( self.progress_bar_window, text="Exporting Labeled Images", padx=20, pady=20, font=("Arial", 16) )
         self.progress_window_label1.grid(row=0, column=0)
-        
+
         self.progress_bar = Progressbar(
                                 self.progress_bar_window,
                                 orient = HORIZONTAL,
@@ -126,4 +134,3 @@ class MainWindow:
 if __name__ == '__main__':
     main_window = MainWindow()
     main_window.run()
-    
