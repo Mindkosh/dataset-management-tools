afeeea8292895fb572a7de19a77f6bf25c952c0a
pranjal2410 <pranjaln2410@outlook.com>
2022-04-26 02:22:02 +0530

    feat: search bar

--- src/gallery.py
+++ src/gallery.py
@@ -1,5 +1,6 @@
 import tkinter
 import os
+from tkinter import messagebox
 import utils
 from PIL import ImageTk, Image
 
@@ -8,12 +9,18 @@ class Gallery:
 
     def __init__(self, img_list, selectImage):
         self.gallery = tkinter.Toplevel()
+        self.img_list = img_list
         self.selectImage = selectImage
         self.gallery.title('Image Gallery')
         self.imageFrame = tkinter.Frame(self.gallery)
-        self.imageFrame.grid(row=0, column=0)
+        self.imageFrame.grid(row=2, column=0)
         self.vbar = tkinter.Scrollbar(self.gallery, orient='vertical')
-        self.vbar.grid(row=0, column=3)
+        self.filenameVar = tkinter.StringVar()
+        self.name_entry = tkinter.Entry(self.gallery, textvariable=self.filenameVar, font=('calibre', 10, 'normal'))
+        self.name_entry.grid(row=0, column=0)
+        self.searchBtn = tkinter.Button(self.gallery, text="Search", command=self.search, bg='#0052cc',
+                                   fg='#ffffff', pady=8, cursor="hand1")
+        self.searchBtn.grid(row=1, column=0)
         # if img_list is not None:
         #     rows = int(len(img_list)/3) + 1
         #     for i in range(0, rows):
@@ -38,12 +45,19 @@ class Gallery:
                 image = ImageTk.PhotoImage(Image.open(img_list[i]).resize((150, 100), Image.ANTIALIAS))
                 label = tkinter.Button(self.imageFrame, image=image, command=lambda c=i: self.select(c))
                 label.photo = image
-                label.grid(row=int(i/3), column=i % 3)
+                label.grid(row=(int(i / 3) + 2), column=i % 3)
                 self.all_labels.append(label)
 
     def select(self, index):
         self.selectImage(index)
         self.gallery.destroy()
+
+    def search(self):
+        for index, name in enumerate(self.img_list):
+            if self.filenameVar.get() in name:
+                self.select(index)
+                return
+        messagebox.showinfo("Error", "Image not found")
 
     def run(self):
         self.gallery.mainloop()
